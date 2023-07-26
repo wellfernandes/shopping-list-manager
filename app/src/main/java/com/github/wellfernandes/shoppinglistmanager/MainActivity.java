39954ef989fca7ffc2f832a70d3e0a398fb7f26e
Welliton Fernandes Leal <wellfernandes@me.com>
2023-07-26 00:30:25 -0300

    adding text view, edit text and button

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/MainActivity.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/MainActivity.java
@@ -3,12 +3,28 @@ package com.github.wellfernandes.shoppinglistmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.github.wellfernandes.shoppinglistmanager.model.ShoppingList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNewList = findViewById(R.id.editTextNewList);
+    }
+
+    public void addNewList(View view){
+        ShoppingList newList = new ShoppingList();
+
+        newList.setName(String.valueOf(editTextNewList.getText()));
+
+        System.out.println("Created list name:: " + newList.getName());
     }
 }
---
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/model/ShoppingList.java
@@ -0,0 +1,54 @@
+package com.github.wellfernandes.shoppinglistmanager.model;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ShoppingList {
+    private int id;
+    private String name;
+    private Date createdAt;
+    private List<Item> itens;
+
+
+    public ShoppingList(){
+
+    }
+    public ShoppingList(int id, String name, Date createdAt, List itens) {
+        this.id = id;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.itens = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<Item> itens) {
+        this.itens = itens;
+    }
+}
