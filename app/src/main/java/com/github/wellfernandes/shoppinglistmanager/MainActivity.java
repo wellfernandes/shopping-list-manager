1645204f17d9e700e7795550007ac58593b7b5af
Welliton Fernandes Leal <wellfernandes@me.com>
2023-07-31 22:04:53 -0300

    added check box element

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/MainActivity.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.wellfernandes.shoppinglistmanager.model.ShoppingList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextNewList;
+    private CheckBox checkBoxLists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTextNewList = findViewById(R.id.editTextNewList);
+        checkBoxLists = findViewById(R.id.checkBoxLists);
+
+        checkBoxListener();
+
     }
 
     public void addNewList(View view){
@@ -25,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
 
         newList.setName(String.valueOf(editTextNewList.getText()));
 
-        System.out.println("Created list name:: " + newList.getName());
+        // show the newly added list
+        showListName(view);
+
+        System.out.println("Created list name: " + newList.getName());
+    }
+
+    public void showListName(View view){
+        String listName = editTextNewList.getText().toString();
+        Toast.makeText(this, listName, Toast.LENGTH_LONG).show();
+    }
+
+    private void checkBoxListener() {
+        checkBoxLists.setOnCheckedChangeListener(
+                (buttonView, isChecked) -> {
+            if (isChecked) {
+                checkBoxLists.setText(R.string.hideLists);
+                Toast.makeText(this, R.string.showingLists, Toast.LENGTH_SHORT).show();
+            } else {
+                checkBoxLists.setText(R.string.showLists);
+                Toast.makeText(this, R.string.hidingLists, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
