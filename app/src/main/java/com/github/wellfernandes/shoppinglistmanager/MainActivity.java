7a548b42f810d1ce63e2d4d38c931ca7583799e4
Welliton Fernandes Leal <wellfernandes@me.com>
2023-08-04 00:46:38 -0300

    added spinner component and field validations

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/MainActivity.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/MainActivity.java
@@ -4,22 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.github.wellfernandes.shoppinglistmanager.model.ShoppingList;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static ArrayList<ShoppingList> allLists = new ArrayList<>();
     private EditText editTextNewList;
     private CheckBox checkBoxLists;
     private RadioGroup radioGroupListFilter;
     private RadioButton radioButtonOpenLists;
     private RadioButton radioButtonCompletedLists;
     private RadioButton radioButtonAllLists;
+
+    private Spinner spinnerLists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +42,50 @@ public class MainActivity extends AppCompatActivity {
         radioButtonCompletedLists = findViewById(R.id.radioButtonCompletedLists);
         radioButtonAllLists = findViewById(R.id.radioButtonAllLists);
 
+        spinnerLists = findViewById(R.id.spinnerLists);
+
         checkBoxListener();
     }
 
     public void addNewList(View view){
-        ShoppingList newList = new ShoppingList();
+        // check all fields
+        if (checkFields(view)){
+            ShoppingList newList = new ShoppingList();
+            newList.setName(String.valueOf(editTextNewList.getText()));
 
-        newList.setName(String.valueOf(editTextNewList.getText()));
+            // add new shopping list
+            allLists.add(newList);
 
-        // show the newly added list
-        showListName(view);
+            // show the newly added list
+            showListName(view);
+            fillSpinnerList();
+        }
 
-        System.out.println("Created list name: " + newList.getName());
+    }
+
+    public boolean checkFields(View view){
+
+        if (editTextNewList.getText().toString().equals("")){
+            editTextNewList.requestFocus();
+            Toast.makeText(this, R.string.messageInsertListName, Toast.LENGTH_SHORT).show();
+            return false;
+        }else if (!checkBoxLists.isChecked() && radioGroupListFilter.getCheckedRadioButtonId() == -1){
+            checkBoxLists.requestFocus();
+            Toast.makeText(this, R.string.messageSelectShowLists, Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(checkBoxLists.isChecked() && radioGroupListFilter.getCheckedRadioButtonId() == -1){
+            Toast.makeText(this, R.string.messageSelectOption, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
     public void clearText(View view){
+
         editTextNewList.setText("");
+        checkBoxLists.setChecked(false);
+        radioGroupListFilter.clearCheck();
+        Toast.makeText(this, R.string.messageCleanFields, Toast.LENGTH_SHORT).show();
     }
 
     public void showListName(View view){
@@ -79,12 +114,37 @@ public class MainActivity extends AppCompatActivity {
         radioGroupListFilter.setOnCheckedChangeListener(
                 (group, checkedId) -> {
             if (checkedId == R.id.radioButtonOpenLists && checkBoxLists.isChecked()) {
-                Toast.makeText(this, "Abertas", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, R.string.openListStatus, Toast.LENGTH_SHORT).show();
             } else if (checkedId == R.id.radioButtonCompletedLists && checkBoxLists.isChecked()) {
-                Toast.makeText(this, "Concluídas", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, R.string.completedListstatus, Toast.LENGTH_SHORT).show();
             } else if (checkedId == R.id.radioButtonAllLists && checkBoxLists.isChecked()) {
-                Toast.makeText(this, "Todas", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, R.string.allListStatus, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void fillSpinnerList(){
+        ArrayList<String> listName = new ArrayList<>();
+
+        for (int i = 0; i < allLists.size(); i++) {
+            listName.add(allLists.get(i).getName());
+            System.out.println("Nome: " + allLists.get(i).getName());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listName);
+        spinnerLists.setAdapter(adapter);
+    }
+
+    public void showSelectedItem(View view){
+        String selectedItem = (String) spinnerLists.getSelectedItem();
+        String message;
+
+        if (selectedItem != null){
+            message = selectedItem + getString(R.string.messageSelectedItem);
+        }else{
+            message = getString(R.string.messageNoSelectedList);
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
