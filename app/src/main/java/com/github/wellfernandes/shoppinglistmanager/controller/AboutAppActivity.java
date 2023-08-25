dce3eba7729ba049be12203796a2aa54979bf18f
Welliton Fernandes Leal <wellfernandes@me.com>
2023-08-24 21:33:18 -0300

    adding new view about software

---
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/controller/AboutAppActivity.java
@@ -0,0 +1,16 @@
+package com.github.wellfernandes.shoppinglistmanager.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.wellfernandes.shoppinglistmanager.R;
+
+public class AboutAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+    }
+}
