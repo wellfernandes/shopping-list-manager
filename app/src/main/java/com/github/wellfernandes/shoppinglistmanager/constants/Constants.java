3c1f1f784ca2918731aef962f3de159a0ec1746f
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-02 19:25:25 -0300

    added new constants

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/constants/Constants.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/constants/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     public static final String EXTRA_NEW_LIST_NAME = "NEW_LIST_NAME";
     public static final String EXTRA_LIST_PRIORITY = "LIST_PRIORITY";
     public static final int REQUEST_CODE = 1;
+    public static final String EXTRA_LIST_ID = "LIST_ID";
+    public static final String EXTRA_LIST_NAME = "LIST_NAME";
+    public static final String EXTRA_EDITED_POSITION = "EDITED_POSITION";
 }
