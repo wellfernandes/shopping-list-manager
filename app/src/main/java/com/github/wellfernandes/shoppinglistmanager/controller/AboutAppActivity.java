8ba5a67bb50d322d09b324c744c1be6f061496c1
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-02 15:43:10 -0300

    added set title in about view

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/controller/AboutAppActivity.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/controller/AboutAppActivity.java
@@ -12,5 +12,7 @@ public class AboutAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+
+        setTitle("Sobre o Aplicativo");
     }
 }
