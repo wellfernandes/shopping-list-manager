24428b9bd0a4826077e23683640e04bc72813d81
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-03 19:19:21 -0300

    added new item selection features

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/controller/ShoppingListActivity.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/controller/ShoppingListActivity.java
@@ -1,9 +1,11 @@
 package com.github.wellfernandes.shoppinglistmanager.controller;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 
 import com.github.wellfernandes.shoppinglistmanager.R;
 import com.github.wellfernandes.shoppinglistmanager.constants.Constants;
@@ -22,9 +25,60 @@ import java.util.Date;
 import java.util.List;
 
 public class ShoppingListActivity extends AppCompatActivity {
+    private int nextItemId = 1;
     private ListView listViewDefault;
     private static List<ShoppingList> shoppingLists = new ArrayList<>();
     private ShoppingListAdapter shoppingListAdapter;
+
+    private ActionMode actionMode;
+    private View selectedView;
+
+    private ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.main_view_context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            if (Constants.SELECTED_POSITION == -1) {
+                return ShoppingListActivity.super.onContextItemSelected(item);
+            }
+
+            if (item.getItemId() == R.id.menuItemEdit) {
+                editList(Constants.SELECTED_POSITION);
+                mode.finish();
+                return true;
+            } else if (item.getItemId() == R.id.menuItemDelete) {
+                deleteList(Constants.SELECTED_POSITION);
+                mode.finish();
+                return true;
+            }
+
+            Constants.SELECTED_POSITION = -1;
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            if(selectedView != null) {
+                selectedView.setBackgroundColor(Color.TRANSPARENT);
+            }
+
+            actionMode = null;
+            selectedView = null;
+
+            listViewDefault.setEnabled(true);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +95,35 @@ public class ShoppingListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ShoppingList clickedList = shoppingLists.get(position);
                 String itemName = clickedList.getName();
+
+                Constants.SELECTED_POSITION = position;
+                view.setSelected(true);
+
                 Toast.makeText(ShoppingListActivity.this, getString(R.string.itemClicked) + itemName, Toast.LENGTH_SHORT).show();
             }
         });
 
         registerForContextMenu(listViewDefault);
+
+        listViewDefault.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listViewDefault.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                if(actionMode != null) {
+                    return false;
+                }
+
+                Constants.SELECTED_POSITION = position;
+                selectedView = view;
+                selectedView.setBackgroundColor(Color.LTGRAY);
+                listViewDefault.setEnabled(false);
+
+                actionMode = startSupportActionMode(actionModeCallback);
+
+                return true;
+            }
+        });
     }
 
     @Override
@@ -94,15 +172,16 @@ public class ShoppingListActivity extends AppCompatActivity {
             String listName = data.getStringExtra(Constants.EXTRA_NEW_LIST_NAME);
             String listPriority = data.getStringExtra(Constants.EXTRA_LIST_PRIORITY);
 
-            ShoppingList newList = new ShoppingList(shoppingLists.size() + 1, listName, new Date(), listPriority);
+            ShoppingList newList = new ShoppingList(nextItemId, listName, new Date(), listPriority);
             shoppingLists.add(newList);
 
-            shoppingListAdapter.notifyDataSetChanged();
+            nextItemId++;
         }
+
+        shoppingListAdapter.notifyDataSetChanged();
     }
 
     private void populateListViewShoppingLists() {
-
         shoppingListAdapter = new ShoppingListAdapter(this, shoppingLists);
         listViewDefault.setAdapter(shoppingListAdapter);
     }
@@ -112,12 +191,20 @@ public class ShoppingListActivity extends AppCompatActivity {
         intent.putExtra(Constants.EXTRA_LIST_ID, shoppingLists.get(position).getId());
         intent.putExtra(Constants.EXTRA_LIST_NAME, shoppingLists.get(position).getName());
         intent.putExtra(Constants.EXTRA_LIST_PRIORITY, shoppingLists.get(position).getPriority());
+        intent.putExtra(Constants.EXTRA_EDITED_POSITION, position);
         startActivityForResult(intent, Constants.REQUEST_CODE);
-
     }
 
     private void deleteList(int position) {
         shoppingLists.remove(position);
         shoppingListAdapter.notifyDataSetChanged();
+    }
+
+    public static List<ShoppingList> getShoppingLists() {
+        return shoppingLists;
+    }
+
+    public static void setShoppingLists(List<ShoppingList> shoppingLists) {
+        ShoppingListActivity.shoppingLists = shoppingLists;
     }
 }
