46ea041e79da7683d0a8331b1efdfd5c941fccc1
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-09 19:16:26 -0300

    added new constants for configuring shared preferences

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/constants/Constants.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/constants/Constants.java
@@ -1,5 +1,7 @@
 package com.github.wellfernandes.shoppinglistmanager.constants;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class Constants {
     public static final String EXTRA_NEW_LIST_NAME = "NEW_LIST_NAME";
     public static final String EXTRA_LIST_PRIORITY = "LIST_PRIORITY";
@@ -8,4 +10,8 @@ public class Constants {
     public static final String EXTRA_LIST_NAME = "LIST_NAME";
     public static final String EXTRA_EDITED_POSITION = "EDITED_POSITION";
     public static int SELECTED_POSITION = -1;
+    public static final String PACKEGE_DOMAIN = "com.github.wellfernandes.shoppinglistmanager";
+    public static final String SHARED_PREFERENCES = PACKEGE_DOMAIN + ".sharedpreferences";
+    public static int DEFAULT_THEME_OPTION = AppCompatDelegate.MODE_NIGHT_NO;
+    public static String APP_THEME = "APP_THEME";
 }
