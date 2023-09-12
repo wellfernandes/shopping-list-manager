ef6818d6a3a7fc363e1622319016dbf817d5fc24
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-11 22:31:54 -0300

    added constants relating to the database

---
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/constants/DatabaseConstants.java
@@ -0,0 +1,7 @@
+package com.github.wellfernandes.shoppinglistmanager.constants;
+
+public class DatabaseConstants {
+    public static final String DATABASE_NAME = "shopping_list_manager.db";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "shopping_list";
+}
