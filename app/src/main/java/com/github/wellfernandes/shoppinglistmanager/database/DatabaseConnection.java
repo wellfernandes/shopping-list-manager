e3c4ee546ae7c5276cce2dddd2303794ccd70677
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-12 23:50:41 -0300

    added database connection

---
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/database/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package com.github.wellfernandes.shoppinglistmanager.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.github.wellfernandes.shoppinglistmanager.constants.DatabaseConstants;
+import com.github.wellfernandes.shoppinglistmanager.model.ShoppingList;
+import com.github.wellfernandes.shoppinglistmanager.model.ShoppingListDAO;
+@Database(entities = {ShoppingList.class}, version = 1, exportSchema = false)
+public abstract class DatabaseConnection extends RoomDatabase {
+    private static DatabaseConnection databaseConnection;
+    public abstract ShoppingListDAO shoppingListDAO();
+    public static synchronized DatabaseConnection getInstance(Context context) {
+        if (databaseConnection == null) {
+            databaseConnection = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            DatabaseConnection.class,
+                            DatabaseConstants.DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return databaseConnection;
+    }
+}
