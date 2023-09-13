e1115686414cfbd6fe973495e2fbc1015bd910f1
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-12 23:49:15 -0300

    added shopping list dao

---
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/model/ShoppingListDAO.java
@@ -0,0 +1,22 @@
+package com.github.wellfernandes.shoppinglistmanager.model;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import androidx.room.Delete;
+import java.util.List;
+@Dao
+public interface ShoppingListDAO {
+    @Insert
+    void insert(ShoppingList shoppingList);
+    @Update
+    void update(ShoppingList shoppingList);
+    @Delete
+    void delete(ShoppingList shoppingList);
+    @Query("SELECT * FROM shoppinglist WHERE id = :id")
+    LiveData<ShoppingList> getShoppingListById(int id);
+    @Query("SELECT * FROM shoppinglist ORDER BY createdAt ASC")
+    LiveData<List<ShoppingList>> getAllShoppingLists();
+}
