2e8c0b2fa55a994a6daeef14d21cbc4ba4a6de76
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-12 23:53:25 -0300

    added database error constants

---
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/constants/ErrorConstants.java
@@ -0,0 +1,6 @@
+package com.github.wellfernandes.shoppinglistmanager.constants;
+
+public class ErrorConstants {
+    public static final String ERROR_DELETE_LIST = "Error deleting list";
+    public static final String ERROR_UPDATE_LIST = "Error updating list";
+}
