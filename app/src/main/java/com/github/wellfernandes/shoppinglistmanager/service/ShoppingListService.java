ba83e251c120bb990efc5ac03f3d821ee61295b9
Welliton Fernandes Leal <wellfernandes@me.com>
2023-09-13 21:13:06 -0300

    renamed shopping list dao to shopping list repository

--- app/src/main/java/com/github/wellfernandes/shoppinglistmanager/service/ShoppingListService.java
+++ app/src/main/java/com/github/wellfernandes/shoppinglistmanager/service/ShoppingListService.java
@@ -1,22 +1,22 @@
-package com.github.wellfernandes.shoppinglistmanager.repository;
+package com.github.wellfernandes.shoppinglistmanager.service;
 
 import android.content.Context;
 import androidx.lifecycle.LiveData;
 
 import com.github.wellfernandes.shoppinglistmanager.database.DatabaseConnection;
 import com.github.wellfernandes.shoppinglistmanager.model.ShoppingList;
-import com.github.wellfernandes.shoppinglistmanager.model.ShoppingListDAO;
+import com.github.wellfernandes.shoppinglistmanager.repository.ShoppingListRepository;
 
 import java.util.List;
 
 public class ShoppingListService {
-    private final ShoppingListDAO shoppingListDao;
+    private final ShoppingListRepository shoppingListRepository;
     private final LiveData<List<ShoppingList>> allShoppingLists;
 
     public ShoppingListService(Context context) {
         DatabaseConnection databaseConnection = DatabaseConnection.getInstance(context);
-        shoppingListDao = databaseConnection.shoppingListDAO();
-        allShoppingLists = shoppingListDao.getAllShoppingLists();
+        shoppingListRepository = databaseConnection.shoppingListRepository();
+        allShoppingLists = shoppingListRepository.getAllShoppingLists();
     }
 
     public LiveData<List<ShoppingList>> getAllShoppingLists() {
@@ -24,6 +24,6 @@ public class ShoppingListService {
     }
 
     public LiveData<ShoppingList> getShoppingListById(int id) {
-       return shoppingListDao.getShoppingListById(id);
+       return shoppingListRepository.getShoppingListById(id);
     }
 }
